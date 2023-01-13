3c4e5ea87a2c4240861886bf562b29f27772e281
Ranjeet Soni <ranjeetsoni1996@gmail.com>
2023-01-13 22:30:57 +0530

    14/01/2023

--- src/Comoponents/SideBar.jsx
+++ src/Comoponents/SideBar.jsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import TollIcon from "@mui/icons-material/Toll";
 import InsertCommentIcon from "@mui/icons-material/InsertComment";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import SearchIcon from "@mui/icons-material/Search";
 import "./sideBar.css";
 import { UserProfile } from "./UserProfile";
+import { useEffect } from "react";
+import db from "../firebase";
 
 export function SideBar({currentUser,signOut}) {
+
+  const [allUsers,setAllUsers]=useState([]);
+  const[searchInput,setSearchInput]=useState("");
+  const [friendList, setFriendList] = useState([]);
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      const data = await db.collection("users").onSnapshot((snapshot) => {
+        setAllUsers(
+          snapshot.docs.filter((doc) => doc.data().email !== currentUser?.email)
+        );
+      });
+    };
+
+    const getFriends = async () => {
+      const data = await db
+        .collection("Friendlist")
+        .doc(currentUser.email)
+        .collection("list")
+        .onSnapshot((snapshot) => {
+          setFriendList(snapshot.docs);
+        });
+    };
+
+    getAllUsers();
+    getFriends();
+  }, []);
+
+  const searchedUser = allUsers.filter((user) => {
+    if (searchInput) {
+      if (
+        user.data().fullname.toLowerCase().includes(searchInput.toLowerCase())
+      ) {
+        return user;
+      }
+    }
+  });
+
+  const searchItem = searchedUser.map((user) => {
+    return (
+      <UserProfile
+        name={user.data().fullname}
+        photoURL={user.data().photoURL}
+        key={user.id}
+        email={user.data().email}
+      />
+    );
+  });
+
+
   return (
     <div className="sidebar">
       <div className="sidebar-header">
@@ -22,10 +74,22 @@ export function SideBar({currentUser,signOut}) {
       <div className="sidebar-search">
         <div className="sidebar-search-input">
             <SearchIcon/>
-            <input type="text" name="seach" placeholder="Search..."/>
+            <input type="text" name="seach" placeholder="Search..."
+              value={searchInput} onChange={e=>setSearchInput(e.target.value)}
+            />
         </div>
       </div>
       <div className="sidebar-chat-list">
+      {
+        searchItem.length>0 ? searchItem : friendList.map((friend)=>(
+          <UserProfile
+                name={friend.data().fullname}
+                photoURL={friend.data().photoURL}
+                lastMessage={friend.data().lastMessage}
+                email={friend.data().email}
+              />
+            ))}
+      
         <UserProfile name="Ranjeet" photoURL="./user.png"/>
         <UserProfile name="Rakhi" photoURL="./user.png"/>
         <UserProfile name="Ajeet" photoURL="./user.png"/>
--- src/Comoponents/UserProfile.jsx
+++ src/Comoponents/UserProfile.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 import "./UserProfile.css"
 
-export function UserProfile({name,photoURL}) {
+export function UserProfile({name,photoURL,email}) {
+  const navigate=useNavigate();
+  const goToUser=(emailId)=>{
+    if(emailId){
+      navigate(`/${emailId}`);
+    }
+  }
   return (
-    <div className='user-profile'>
+    <div className='user-profile' onClick={()=>goToUser(email)}>
         {/* img of user */}
         <div className="user-image">
             <img src={photoURL} alt="" />
